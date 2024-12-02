326e649cfece5ca8403789a52cf13f4462cb456b
MiguelAlmeidaDias <0000110192004Xsp@al.educacao.sp.gov.br>
2024-12-02 18:55:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'Geografia',
+    'Pergunta: Qual é a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é a fotossíntese?',
+    'É o processo pelo qual as plantas, algas e algumas bactérias convertem luz solar em energia química, usando dióxido de carbono e água para produzir glicose e oxigênio.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Casmurro"?',
+    'Machado de Assis'
+)
+
+
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'H₂O'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um triângulo equilátero?',
+    'É um triângulo em que todos os três lados têm o mesmo comprimento e todos os ângulos são iguais, medindo 60 graus cada.)'
+)
